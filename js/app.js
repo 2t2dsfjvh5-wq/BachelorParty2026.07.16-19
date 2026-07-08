c23e0615e6b21b936cae72bbe9ecdc9fb2bc8a28
2t2dsfjvh5-wq <2t2dsfjvh5@privaterelay.appleid.com>
2026-07-08 22:29:24 +0200

    Přidán app.js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const app = document.getElementById("app");
+
+app.innerHTML = `
+<div class="boot">
+
+    <div class="logo">
+        OPERATION
+        <br>
+        <span>LAST COMMIT</span>
+    </div>
+
+    <div class="loading">
+        Initializing secure environment...
+    </div>
+
+</div>
+`;
